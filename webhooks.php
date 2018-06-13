ab6963e9af0f79008d6e7d4b563e32a72f92095f
Patiphol pussawong <vectorx2263@hotmail.com>
2018-06-13 19:12:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,9 +25,15 @@ if (!is_null($events['events'])) {
 
 				// Build message to reply back
 				$messages = [
-					'type' => 'image',
-					'originalContentUrl' => $imgurl,
-					'previewImageUrl' => $imgurl
+					[
+						'type' => 'text',
+						'text' => 'เมี้ยวว'
+					],
+					[
+						'type' => 'image',
+						'originalContentUrl' => $imgurl,
+						'previewImageUrl' => $imgurl
+					]
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
